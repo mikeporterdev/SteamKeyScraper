effe382b84610e4b9296e5a98760c2aac25cdb4e
Michael Porter <mporter271@gmail.com>
2014-11-01 19:11:51 +0000

    Fixed caching issue

--- main.py
+++ main.py
@@ -5,39 +5,47 @@ Created on 1 Nov 2014
 '''
 import praw
 import re
-#from pushbullet import PushBullet
-from settings import api_key
-    
+import time
 
+from settings import api_key
+
+
+#from pushbullet import PushBullet
 #def pushToDevices(title, key):
 #    pb = PushBullet(api_key)
 #    phone = pb.push_note(title, key)
 #    print(phone.status_code)
-    
 def getKey(post):
     code = re.findall(r'[A-Z0-9]+-[A-Z0-9]+-[A-Z0-9]+', post)  
     return code
 
 def scrapeSubreddit(connection, subreddit):
 
-    submissions = connection.get_subreddit(subreddit).get_new(limit=100)
-    
+    submissions = connection.get_subreddit(subreddit).get_new(limit=2)
+    keysList = []
     for submission in submissions:
         post = vars(submission)
         body = post['selftext']
         keys = getKey(body)
         if keys:      
-            #pushToDevices(str(submission), keys)     
-            print(str(submission))
-            print(keys)
+            #pushToDevices(str(submission), keys)
+            keysList.append(({str(submission) : keys}))
+    return keysList
+    
+
 
 def main():
     r = praw.Reddit(user_agent = "Steam Key Scraper")
-    subreddits = ['pcgaming', 'gaming', 'steam', 'games']
-    for subreddit in subreddits:
-        print(subreddit)
-        scrapeSubreddit(r, subreddit)
-
+    keys = []
+    while True:
+        subreddits = ['games', 'gaming', 'pcgaming', 'steam', 'evolvekeys']
+        for subreddit in subreddits:
+            key = scrapeSubreddit(r, subreddit)
+            if key:
+                if not key in keys:
+                    keys.append(key)
+                    print(key)
+        time.sleep(31)
 
 if __name__ == '__main__':
     main()
